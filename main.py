df0af917307513c8ac08f51a99c6aab683b55edd
Jessica Patricio <jessicacacau@gmail.com>
2024-02-10 02:00:03 -0300

    add docstrings

--- main.py
+++ main.py
@@ -1,21 +1,33 @@
+import sys
+
+
 def get_file_content(file_path):
+    """ Extracts the contents of a file. """
+
     with open(file_path) as f:
-        file_content = f.read()
+            file_content = f.read()
+    
     return file_content
 
 
 def count_words(text):
+    """ Counts the number of words in a string. """
+
     words = text.split()
     num_words = len(words)
     return num_words
 
 
 def sort_dict_by_value(unordered):
+    """ Sorts a dict in descending order based on its values. """
+
     ordered = dict(sorted(unordered.items(), key=lambda d: d[1], reverse=True))
     return ordered
 
 
 def count_characters(text):
+    """ Counts the number of times all the letters appear on a text. """
+
     lower_text = text.lower()
     num_characters = {}
 
@@ -30,25 +42,21 @@ def count_characters(text):
     return num_characters
 
 
-def tab():
-    return "  "
-
-
 def print_report(file_path, num_words, num_characters):
-    t = tab()
+    """ Shows a book report in stdout. """
 
     print("---")
     print(f"Begin of book report for {file_path}")
     print()
-    print(f"{t}Number of words on the file")
+    print(f"\tNumber of words:")
     print()
-    print(f"{t}{t}{num_words}")
+    print(f"\t\t{num_words}")
     print()
-    print("  Characters found and how many times in descending order")
+    print("\tList of most used letters:")
     print()
     
     for character in num_characters:
-        print(f"    {character}: {num_characters[character]}")
+        print(f"\t\t{character}: {num_characters[character]}")
     pass
 
     print()
@@ -57,7 +65,13 @@ def print_report(file_path, num_words, num_characters):
 
 
 def main():
-    file_path = "books/frankenstein.txt"
+    """ Runs the program. """
+
+    if len(sys.argv) > 1:
+        file_path = sys.argv[1]
+    else:
+        raise Exception("Command format: python3 bookbot.py path/to/book/file/")
+    
     file_content = get_file_content(file_path)
     num_words = count_words(file_content)
     num_characters = count_characters(file_content)
